bdfe9749f2deca598263b36a2d8d296598eec6e3
DylMacK <74017513+DylMacK@users.noreply.github.com>
2020-11-05 14:51:17 -0500

    Update app.py
    removed unnecessary imports

--- app.py
+++ app.py
@@ -2,12 +2,10 @@
 
 import os
 from flask import Flask
-from flask import request
 from flask import render_template
 import pandas as pd
 import folium
 from folium.plugins import HeatMap
-import csv
 
 
 app = Flask(__name__)
