0028acbff601c4b34543cc8d127e10fd33427d35
DylMacK <74017513+DylMacK@users.noreply.github.com>
2020-11-10 14:43:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -36,20 +36,19 @@ def display_survey_results():
     return render_template('view_survey_results.html')
 
 @app.route('/drawMap')
-def draw_map():
-
-    map_data = pd.read_csv("./Data/data_01.csv", sep=';')
-    lat = map_data['LATITUDE'].mean()
-    lon = map_data['LONGITUDE'].mean()
-    startingLocation = [lat, lon]  # [39.47, -0.37]
-    hmap = folium.Map(location=startingLocation, zoom_start=15)
-    max_amount = map_data['PricePerSquareMeters'].max()
+draw_map():
+    map_data = pd.read_csv('./Data/us-zip-codes-cleaned.csv')
+    lat = map_data['Lat'].mean()
+    lon = map_data['Long'].mean()
+    startingLocation = [lat, lon]  # [40.05, 74.40]
+    hmap = folium.Map(location=startingLocation, zoom_start=8)
+    max_amount = map_data['Risk'].max()
     hm_wide = HeatMap(
-        list(zip(map_data.LATITUDE.values, map_data.LONGITUDE.values, map_data.PricePerSquareMeters.values)),
-        min_opacity=0.2,
+        list(zip(map_data.Lat.values, map_data.Long.values, map_data.Risk.values)),
+        min_opacity=0.05,
         max_val=max_amount,
-        radius=17, blur=15,
-        max_zoom=1)
+        radius=25, blur=15,
+        max_zoom=4)
 
     # Adds the heatmap element to the map
     hmap.add_child(hm_wide)
@@ -57,6 +56,5 @@ def draw_map():
     hmap.save(os.path.join('./templates', 'heatmap.html'))
     # Render the heatmap
     return render_template('heatmap.html')
-
 
 
